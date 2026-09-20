4cff30c9c2516eb7a7aa007c7a4b6c408ea18e7f
rokeyaag <lutforitsolution@gmail.com>
2026-09-20 15:34:30 +0600

    fix(vercel): move local-server to scripts and ignore it to ensure pure static deployment without serverless functions

--- scripts/local-server.js
+++ scripts/local-server.js
@@ -35,7 +35,8 @@ const server = http.createServer((req, res) => {
     reqPath = '/index.html';
   }
 
-  const filePath = path.join(__dirname, reqPath);
+  const rootDir = path.resolve(__dirname, '..');
+  const filePath = path.join(rootDir, reqPath);
 
   fs.stat(filePath, (err, stats) => {
     if (err || !stats.isFile()) {
